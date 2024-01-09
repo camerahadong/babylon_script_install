8f7f6013d44d4fd34b91c1c1cca90c10500a6837
camerahadong <134601189+camerahadong@users.noreply.github.com>
2024-01-09 10:03:43 +0700

    Update babylon_script_install.sh

--- babylon_script_install.sh
+++ babylon_script_install.sh
@@ -3,14 +3,14 @@
 # Bước 1: Cập nhật hệ thống
 sudo apt update && sudo apt upgrade -y
 
-# Bước 2: Cài đặt công cụ xây dựng
-sudo apt -qy install curl git jq lz4 build-essential
+# Bước 2: Cài đặt công cụ xây dựng và các gói cần thiết
+sudo apt -y install curl git jq lz4 build-essential
 
 # Bước 3: Cài đặt Go
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.20.12.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
-eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+curl -Ls https://golang.org/dl/go1.20.12.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
+export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 # Bước 4: Sao chép mã nguồn và xây dựng
 cd $HOME
@@ -30,15 +30,15 @@ sudo ln -s $HOME/.babylond/cosmovisor/genesis $HOME/.babylond/cosmovisor/current
 sudo ln -s $HOME/.babylond/cosmovisor/current/bin/babylond /usr/local/bin/babylond -f
 
 # Bước 7: Cài đặt và thiết lập Cosmovisor
-go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
-sudo tee /etc/systemd/system/babylon.service > /dev/null << EOF
+go install github.com/cosmos/cosmos-sdk/cmd/cosmovisor@latest
+cat << EOF | sudo tee /etc/systemd/system/babylon.service > /dev/null
 [Unit]
 Description=babylon node service
 After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which cosmovisor) run start
+ExecStart=$(which cosmovisor) start
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=65535
@@ -73,7 +73,7 @@ sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.
 sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
 
 # Bước 9: Tải về bản snapshot mới nhất
-curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
+curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -I lz4 -xf - -C $HOME/.babylond
 [[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
 
 # Bước 10: Khởi động dịch vụ và kiểm tra log
